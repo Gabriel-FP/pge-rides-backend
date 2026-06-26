54b0cfc2235e392bc200b5b397a5f4d04c58db2b
Gabriel-FP <gabrielfelixpaim@gmail.com>
2026-06-26 20:44:08 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/br/gov/pge/rides/RidesBackendApplication.java
@@ -0,0 +1,13 @@
+package br.gov.pge.rides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/pge/rides/RidesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.pge.rides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
